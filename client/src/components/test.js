531fcf0999eca31dd2c2a0f8425ded25ff7c8831
Jordan Salisbury <salisburyjordan21@gmail.com>
2019-01-02 13:28:28 -0800

    Project setup in progress

---
+++ client/src/components/test.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+
+class Test extends Component {
+    async componentDidMount(){
+        const message = {
+            text: "This is a message from the client,Hello!",
+            name: "Bob Saget"
+        }
+
+        const resp = await axios.post('/api/send-message', message);
+
+        console.log('Send message Response:',resp); 
+
+
+
+        // const resp = await axios.get('/api/test');
+        // console.log('Test Response', resp); 
+
+        // const user = await axios.get('/api/user');
+        // console.log('User Response:' , user)
+    }
+    render(){
+        return(
+            <div>
+                <h1>Test Components</h1>
+            </div>
+        ); 
+    }
+}
+
+export default Test; 
